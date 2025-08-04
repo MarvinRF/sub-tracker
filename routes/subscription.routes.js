fb654a6e1a059b7bf38b54e95a102f1efc97cef7
Marvin Rocha <marvinvca@outlook.com>
2025-08-03 21:02:44 -0300

    `Refactor arcjet middleware and subscription routes`

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,21 +1,37 @@
-import {Router} from "express";
+import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
+import { createSubscription } from "../controllers/subsctiption.controller.js";
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.get("/", (req, res) => res.send({title: "GET All subscriptions"}))
+subscriptionRouter.get("/", (req, res) =>
+  res.send({ title: "GET All subscriptions" })
+);
 
-subscriptionRouter.get("/:id", (req, res) => res.send({title: "GET subscriptions details"}))
+subscriptionRouter.get("/:id", (req, res) =>
+  res.send({ title: "GET subscriptions details" })
+);
 
-subscriptionRouter.post("/", (req, res) => res.send({title: "CREATE subscriptions"}))
+subscriptionRouter.post("/", authorize, createSubscription);
 
-subscriptionRouter.put("/", (req, res) => res.send({title: "UPDATE subscriptions"}))
+subscriptionRouter.put("/", (req, res) =>
+  res.send({ title: "UPDATE subscriptions" })
+);
 
-subscriptionRouter.delete("/", (req, res) => res.send({title: "DELETE subscription"}))
+subscriptionRouter.delete("/", (req, res) =>
+  res.send({ title: "DELETE subscription" })
+);
 
-subscriptionRouter.get("/user/:id", (req, res) => res.send({title: "GET All user subscriptions"}))
+subscriptionRouter.get("/user/:id", (req, res) =>
+  res.send({ title: "GET All user subscriptions" })
+);
 
-subscriptionRouter.put("/:id/cancel", (req, res) => res.send({title: "Cancel subscriptions"}))
+subscriptionRouter.put("/:id/cancel", (req, res) =>
+  res.send({ title: "Cancel subscriptions" })
+);
 
-subscriptionRouter.get("/upcoming-renewals", (req, res) => res.send({title: "GET upcoming renewals"}))
+subscriptionRouter.get("/upcoming-renewals", (req, res) =>
+  res.send({ title: "GET upcoming renewals" })
+);
 
 export default subscriptionRouter;
