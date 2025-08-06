e6b9a4671cb4339eb45aed91dab23a45d62e6c03
Marvin Rocha <marvinvca@outlook.com>
2025-08-06 00:30:27 -0300

    `feat: updated environment variables, email password, and workflow controller`

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -4,15 +4,15 @@ import { SERVER_URL } from "../config/env.js";
 
 export const createSubscription = async (req, res, next) => {
   try {
-    const subscrption = await Subscription.create({
+    const subscription = await Subscription.create({
       ...req.body,
       user: req.user._id,
     });
 
-    await workflowClient.trigger({
+    const { workflowRunId } = await workflowClient.trigger({
       url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
       body: {
-        subscrptionId: subscrption.id,
+        subscriptionId: subscription.id,
       },
       headers: {
         "content-type": "application/json",
@@ -20,25 +20,28 @@ export const createSubscription = async (req, res, next) => {
       retries: 0,
     });
 
-    res.status(201).json({ sucess: true, data: subscrption });
-  } catch (error) {
-    next(error);
+    res
+      .status(201)
+      .json({ success: true, data: { subscription, workflowRunId } });
+  } catch (e) {
+    next(e);
   }
 };
 
 export const getSubscriptions = async (req, res, next) => {
   try {
-    if (req.user._id !== req.params.userId) {
-      const error = new Error("You are not the owner of this subscription");
+    // Check if the user is the same as the one in the token
+    if (req.user.id !== req.params.id) {
+      const error = new Error("You are not the owner of this account");
       error.status = 401;
       throw error;
     }
 
-    const subscriptions = await Subscription.find({ user: req.user._id });
+    const subscriptions = await Subscription.find({ user: req.params.id });
 
-    res.status(200).json({ sucess: true, data: subscriptions });
-  } catch (error) {
-    next(error);
+    res.status(200).json({ success: true, data: subscriptions });
+  } catch (e) {
+    next(e);
   }
 };
 
